b3d7cb1cc77c1156d0087d7e90ade4d1e6e89c27
Abdul Adnan <peerabduladnan@gmail.com>
2026-02-25 22:20:05 +0530

    Completed hero Section

--- Positive/src/components/Home.jsx
+++ Positive/src/components/Home.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        <div className='m-8'>
-        <h1 className='text-4xl'>Navigate the digital landscape for success</h1>
+    <section className='mx-auto max-w-7xl mt-17.5 px-5'>
+        <div className='flex flex-col md:flex-row lg:items-center gap-16 lg:gap-24'>
+
+        <div className='order-2 md:order-1 flex flex-col  gap-6 lg:gap-8.75'>
+        <h1 className='font-medium sm:text-4xl md:text-5xl text-3xl lg:text-[60px] leading-[1.1] max-w-125'>Navigate the <br /> digital landscape <br /> for success</h1>
+        <p className='lg:text-[20px] text-sm sm:text-base leading-6 lg:leading-7 max-w-117.5 '>Our digital marketing agency helps businesses <br /> grow and succeed online through a range of <br /> services including SEO, PPC, social media marketing, <br /> and content creation.</p>
+        <button className='w-fit py-4 px-7 lg:py-5 lg:px-8.75 left-0 bg-black text-white rounded-xl'>Book a Consulation</button>
+        </div>
+        <div className='order-1 md:order-2'>
+           <img src="/Illustration.png" alt="Sound" />
         </div>
         </div>
+
+        </section>
   )
 }
 
--- Positive/src/components/Navbar.jsx
+++ Positive/src/components/Navbar.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(null)
+
     return (
         <nav className="mt-8 max-w-360 mx-auto h-17.5 px-5 sm:px-8 lg:px-25 flex items-center justify-between">
 
             {/* Logo */}
-           <div className="flex items-center gap-2 sm:gap-3 font-space">
-  <img
-    src="/Icon.png"
-    className="w-6 h-6 sm:w-7 sm:h-7 lg:w-9 lg:h-9"
-    alt="logo"
-  />
+            <div className="flex items-center gap-2 sm:gap-3 font-space">
+                <img
+                    src="/Icon.png"
+                    className="w-6 h-6 sm:w-7 sm:h-7 lg:w-9 lg:h-9"
+                    alt="logo"
+                />
 
-  <h1 className=" font-medium 
+                <h1 className=" font-medium 
                  text-lg sm:text-xl md:text-2xl lg:text-[32px]">
-    Positivus
-  </h1>
-</div>
+                    Positivus
+                </h1>
+            </div>
 
             {/* Desktop Links */}
             <ul className="hidden lg:flex items-center gap-8">
@@ -33,10 +37,25 @@ const Navbar = () => {
             </button>
 
             {/* Mobile Menu Icon */}
-            <div className="lg:hidden text-2xl">
-                ☰
-            </div>
+            <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden text-2xl">
+                {menuOpen ? "✕" : "☰"}
 
+            </button>
+            {
+                menuOpen && (
+                    <div className="top-20 absolute left-0 w-full bg-white">
+                        <ul className="flex flex-col items-center gap-8 py-8">
+                            <li><a href="#" className="text-black text-lg">About us</a></li>
+                            <li><a href="#" className="text-black text-lg">Services</a></li>
+                            <li><a href="#" className="text-black text-lg">Use Cases</a></li>
+                            <li><a href="#" className="text-black text-lg">Pricing</a></li>
+                            <li><a href="#" className="text-black text-lg">Blog</a></li>
+                        </ul>
+                    </div>
+
+                )
+
+            }
         </nav>
     );
 };
